571948b918a171e17c8ec2b4792f07d2784c80d3
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-17 22:23:03 +0545

    feat: add security settings configuration for JWT generation

--- app/config.py
+++ app/config.py
@@ -20,7 +20,14 @@ class Setting(BaseSettings):
     def POSTGRES_URL(self):
         return f"postgresql+asyncpg://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
+class SecuritySettings(BaseSettings):
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
+
+    model_config = _base_config
+
 db_settings = Setting()
+security_settings = SecuritySettings()
 
 print(db_settings.POSTGRES_URL)
 print(db_settings.POSTGRES_SERVER)
