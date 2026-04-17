2928d9502c7d6f231a1cb78ae19803b71e623b89
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-17 22:23:32 +0545

    feat: implement user registration and login API endpoints

--- app/api/routers/user.py
+++ app/api/routers/user.py
@@ -1,14 +1,27 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app.api.dependencies import UserServiceDep
+from app.api.schemas.user import UserCreate, UserRead
 
 router = APIRouter(prefix='/user', tags=["User"])
 
-@router.get("/{id}")
-async def get_task(id: int):
-    pass
+@router.post("/signup", response_model=UserRead)
+async def register_user(user: UserCreate, service: UserServiceDep):
+    return await service.add(user)
 
-@router.post("/user")
-async def add_task(id: int):
-    pass
+@router.post("/login")
+async def login_user(request_form: Annotated[OAuth2PasswordRequestForm, Depends()], service: UserServiceDep):
+    token = await service.token(
+        request_form.username, request_form.password
+    )
+
+    return {
+        "token": token,
+        "type": "jwt"
+    }
 
 @router.patch("/{id}")
 async def update_task(id: int):
