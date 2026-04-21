1d2b686cbf38c7df573dacd55fd7ba032d4450d4
santosh-sunuwar-mukhiya <santoshsunuwarmukhiya@gmail.com>
2026-04-21 13:38:20 +0545

    refactor(dependencies): add OAuth2 dependencies and prepare auth utilities
    - Import oauth2_scheme and decode_access_token helpers
    - Add commented template for future get_access_token dependency
    - Improve imports organization

--- practice/api/dependencies.py
+++ practice/api/dependencies.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from typing import Annotated
 
 from fastapi import Depends
@@ -6,8 +7,24 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from practice.databases.session import get_session
 from practice.services.seller import SellerService
 from practice.services.shipment import ShipmentService
+from practice.core.security import oauth2_scheme
+from practice.utils import decode_access_token
 
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+
+# access token dep
+# def get_access_token(
+#     token: Annotated[str, Depends(oauth2_scheme)], session: SessionDep
+# ) -> str:
+
+#     data = decode_access_token(token)
+
+#     if data is None:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid access token."
+#         )
+
 
 def get_shipment_service(session: SessionDep):
     return ShipmentService(session)
